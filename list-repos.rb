b6b98314053e72c33481516759c443ba9a74df1e
Martijn Koster <mak-github@greenhills.co.uk>
2018-01-12 19:40:51 +0000

    spacing

--- list-repos.rb
+++ list-repos.rb
@@ -4,7 +4,7 @@ token = File.read("/token")
 
 Octokit.auto_paginate = true
 client = Octokit::Client.new(:access_token => token)
-ARGV.each do|org|
+ARGV.each do |org|
   repos = client.org_repos(org)
   repos.each do |repo|
     puts repo.url
